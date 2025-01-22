33fe24051fdfc6b48b32178f4c5b0e8900b017fc
Erick02406 <erickamihun@gmail.com>
2025-01-22 15:55:32 -0600

    Update and rename Cheche to IniciativaInfierno.java

--- IniciativaInfierno.java
+++ IniciativaInfierno.java
@@ -1,6 +1,6 @@
 import java.util.Scanner
 
-public class Infierno {
+public class IniciativaInfierno {
  public static void main(String[] args){
   Scanner scanner = new Scanner(System.in);
      system.out.println("Hola mundo");
