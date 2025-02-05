6c5e82d222ec23201ef7d95dc13699df90a125e9
Erick02406 <erickamihun@gmail.com>
2025-02-05 14:39:39 -0600

    ProyectoFinal.java

---
+++ ProyectoFinal.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class ProyectoFinal {
+public static void main(String[] args) {
+JFrame frame = new JFrame("Mi primera Ventana");
+  frame.setSize(width:500, height:300);
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+  frame.setLayout(manager:null);
+
+  JLabel etiqueta_anuncio = new JLabel(" ¡Hola, somos Team Infierno! ");
+  JLabel etiqueta_2 = new JLabel("Prueba");
+  JLabel etiqueta_3 = new JLabel("Prueba encima 1");
+  JLabel etiqueta_4 = new JLabel("Prueba encima 2");
+  JButton button = new JButton ("Enviar");
+  JLabel nombre_usuario = new  JLabel ("Nombre de Usuario");
+  JTextField textField = new JTextField(columns:20);
+  
+}
+}
